164b7d19d38093df7b32ce136863520b1888b6e6
Nikolexion <nfalc2003@gmail.com>
2023-09-26 22:13:10 -0400

    Preliminares de las clases Cliente y Direccion

--- untitled/src/Cliente.java
+++ untitled/src/Cliente.java
@@ -1,3 +1,35 @@
 public class Cliente {
+    private String nombre;
+    private String rut;
+    private Direccion direccion;
 
+    public Cliente(String nombre, String rut, String direccion) {
+        this.nombre = nombre;
+        this.rut = rut;
+        this.direccion.setDireccion(direccion);
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getRut() {
+        return rut;
+    }
+
+    public void setRut(String rut) {
+        this.rut = rut;
+    }
+
+    public Direccion getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(Direccion direccion) {
+        this.direccion = direccion;
+    }
 }
---
+++ untitled/src/Direccion.java
@@ -0,0 +1,14 @@
+public class Direccion {
+        private String direccion;
+        public Direccion(String direccion){
+            this.direccion = direccion;
+        }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+}
