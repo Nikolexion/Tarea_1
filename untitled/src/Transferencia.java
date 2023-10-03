fca44735f5367fddfe8e516e68c07eaf6960a7b0
Nikolexion <nfalc2003@gmail.com>
2023-10-03 11:07:47 -0400

    Borrado el Date de los constructores

--- untitled/src/Transferencia.java
+++ untitled/src/Transferencia.java
@@ -4,8 +4,8 @@ class Transferencia extends Pago {
     private String banco;
     private String numCuenta;
 
-    public Transferencia(float monto, Date fecha, String banco, String numCuenta) {
-        super(monto, fecha);
+    public Transferencia(float monto,String banco, String numCuenta) {
+        super(monto);
         this.banco = banco;
         this.numCuenta = numCuenta;
     }
