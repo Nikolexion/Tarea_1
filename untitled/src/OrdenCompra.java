4a029e11b876193a03d9e3be2254e100b2700537
lulunkaii <sofyiaolate@gmail.com>
2023-10-06 22:03:10 -0300

    Cambio en metodos OrdenCompra

--- untitled/src/OrdenCompra.java
+++ untitled/src/OrdenCompra.java
@@ -4,33 +4,39 @@ import java.util.Date;
 public class OrdenCompra {
     private Date fecha;
     private String estado;
-    private ArrayList<DocTributario> tributarioArrayList;
-    //private ArrayList<DetalleOrden> ordenArraylist;
     private Pago pago;
+    private ArrayList<DocTributario> tributarioArrayList;
+    private ArrayList<DetalleOrden> detalleordenArraylist;
 
-    public OrdenCompra(String estado, Pago pago) {
+
+    public OrdenCompra(String estado, Pago pago) { //según enunciado no debería ser estado, fecha??
         fecha = new Date();
         this.estado = estado;
         this.pago = pago;
+
+        //Inicializacion de Arraylist
+        tributarioArrayList = new ArrayList<>();
+        detalleordenArraylist = new ArrayList<>();
     }
 
-    public void calcPrecioSinIVA(){
+    public float calcPrecioSinIVA(){
 
     }
-    public void calcIVA(){
+    public float calcIVA(){
 
     }
-    public void calcPrecio(){
+    public float calcPrecio(){
 
     }
-    public void calcPeso(){
+    public float calcPeso(){
 
     }
+
+    //Getters y setters-------------------------------------------------------------------------------------------------
 
     public Date getFecha() {
         return fecha;
     }
-
     public void setFecha(Date fecha) {
         this.fecha = fecha;
     }
@@ -38,10 +44,18 @@ public class OrdenCompra {
     public String getEstado() {
         return estado;
     }
-
     public void setEstado(String estado) {
         this.estado = estado;
     }
+
+    public Pago getPago() {
+        return pago;
+    }
+    public void setPago(Pago pago) {
+        this.pago = pago;
+    }
+
+    //Funciones para modificar ArrayList Documentos Tributarios---------------------------------------------------------
 
     public DocTributario getTributarioArrayList() {
         return tributarioArrayList.remove(0);
@@ -51,11 +65,12 @@ public class OrdenCompra {
         this.tributarioArrayList.add(NuevoDoc);
     }
 
-    public Pago getPago() {
-        return pago;
+    //Funciones para modificar ArrayList Detalles de Orden
+    public DetalleOrden getDetalleOrdenArraylist(DetalleOrden NuevoDetalle){
+        return this.detalleordenArraylist.remove(0);
+    }
+    public void setDetalleOrdenArraylist(DetalleOrden NuevoDetalle){
+        this.detalleordenArraylist.add(NuevoDetalle);
     }
 
-    public void setPago(Pago pago) {
-        this.pago = pago;
-    }
 }
