accf93166615fce58a08d37d46d30e47d62f5a3d
lulunkaii <sofyiaolate@gmail.com>
2023-10-06 22:57:51 -0300

    Agregando casos de prueba en main y bugfix de detalleOrden

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
-    public static void Main(String[] args){
+    public static void main(String[] args){
         Pago transferencia = new Transferencia(9000, "Santander", "1");
         OrdenCompra ordenCompra = new OrdenCompra("En proceso",transferencia);
-        
+        Cliente cliente = new Cliente("Tulio", "32132-1", "arco udec");
+        Direccion direccion = new Direccion("ola 8392");
+        System.out.println(cliente.toString());
     }
 }
