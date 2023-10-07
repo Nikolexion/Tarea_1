5e9e534cf974ee070ad9c749516942f8dbdc23c0
lulunkaii <sofyiaolate@gmail.com>
2023-10-06 22:08:45 -0300

    Bugfix: arraylist duplicados en OrdenCompra

--- untitled/src/OrdenCompra.java
+++ untitled/src/OrdenCompra.java
@@ -6,11 +6,9 @@ public class OrdenCompra {
     private String estado;
 
     private ArrayList<DocTributario> tributarioArrayList;
-    private ArrayList<DetalleOrden> ordenArraylist;
+    private ArrayList<DetalleOrden> detalleordenArraylist;
 
     private Pago pago;
-    private ArrayList<DocTributario> tributarioArrayList;
-    private ArrayList<DetalleOrden> detalleordenArraylist;
 
 
     public OrdenCompra(String estado, Pago pago) { //según enunciado no debería ser estado, fecha??
@@ -24,16 +22,16 @@ public class OrdenCompra {
     }
 
     public float calcPrecioSinIVA(){
-
+        return 0;
     }
     public float calcIVA(){
-
+        return 0;
     }
     public float calcPrecio(){
-
+        return 0;
     }
     public float calcPeso(){
-
+        return 0;
     }
 
     //Getters y setters-------------------------------------------------------------------------------------------------
